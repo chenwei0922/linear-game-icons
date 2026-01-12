ddf8d98ba00d6f8dcbb2d12e5cf6ce400e68f66b
chenwei <chen158_7@126.com>
2026-01-12 12:48:45 +0800

    feat: debug deploy

--- example/react-next-demo/next.config.ts
+++ example/react-next-demo/next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export' // 关键：启用静态导出
+  output: 'export', // 关键：启用静态导出
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: false, // 确保 URL 以斜杠结尾
+  basePath: process.env.NODE_ENV === 'production' ? '/linear-game-icons' : ''
 };
 
 export default nextConfig;
